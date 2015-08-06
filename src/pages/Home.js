833e57bcdcc233589155b939cdbaef03f56f89c9
biangang.bg <biangang.bg@alibaba-inc.com>
2015-08-06 17:17:23 +0800

    Update 2015-08-06T09:16:50.593Z

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var Router = require('react-router');
+
+
+var Home = React.createClass({
+	displayName: 'VIEW_Home',
+
+	render () {
+		return (
+			<div className="demo-box">
+				<div>Home</div>
+			</div>
+		)
+	}
+});
+
+module.exports = Home;
