72b5d94e1f9d94bbf58c4b642be54e97609432fc
macisi <macisi528@gmail.com>
2015-10-23 14:14:12 +0800

    up

--- nico.production.js
+++ nico.production.js
@@ -1,5 +1,6 @@
 var nicoConfig = require('./nico');
 
-nicoConfig.site.urlPrefix = '/uxcore';
+// nicoConfig.site.urlPrefix = '/uxcore';
+nicoConfig.site.urlPrefix = '';
 
 module.exports = nicoConfig;
