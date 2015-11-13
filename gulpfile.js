3c2a009b3c288b6fb359295f8b3fe71c91190558
光弘 <guanghong.wsj@alibaba-inc.com>
2015-11-13 10:09:12 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('less_index', function(){
         .pipe(less({
             plugins: [autoprefix, LessPluginInlineUrls]
         }))
-        .pipe(minifyCss())
+        .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(gulp.dest('./theme/static/style/'));
 });
 
