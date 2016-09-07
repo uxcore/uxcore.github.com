d1e681c6886c9e2922cbd5118925bf086450a164
光弘 <guanghong.wsj@alibaba-inc.com>
2016-09-07 14:59:31 +0800

    change urlPrefix to loacl

--- nico.production.js
+++ nico.production.js
@@ -1,6 +1,6 @@
 var nicoConfig = require('./nico');
 
-// nicoConfig.site.urlPrefix = '/uxcore';
-nicoConfig.site.urlPrefix = '//g.alicdn.com/uxcore/site/0.0.1';
+nicoConfig.site.urlPrefix = '';
+// nicoConfig.site.urlPrefix = '//g.alicdn.com/uxcore/site/0.0.1';
 
 module.exports = nicoConfig;
